5787220055d3bce65dd2d5e282f6d859430b858c
Kien Nguyen <83683857+sykienbmt@users.noreply.github.com>
2021-10-20 17:39:34 +0700

    email

--- src/panel/NhanVienPnl.java
+++ src/panel/NhanVienPnl.java
@@ -19,9 +19,11 @@ import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
@@ -711,12 +713,12 @@ public class NhanVienPnl extends javax.swing.JPanel {
                 public void actionPerformed(ActionEvent e) {
                     int clickThem = JOptionPane.showConfirmDialog(new Frame(),"Bạn có muốn gửi không ?", "Thông báo",JOptionPane.YES_NO_OPTION);
                     if (clickThem == JOptionPane.YES_OPTION) {
-                        MailSender mail = new MailSender();
+//                        MailSender mail = new MailSender();
                         Properties prop = new Properties();
                         prop.put("mail.smtp.host", "smtp.gmail.com");
                         prop.put("mail.smtp.port", "587");
                         prop.put("mail.smtp.auth", "true");
-                        prop.put("mail.smtp.starttls.enable", "true");
+                        prop.put("mail.smtp.starttls.enable", "true"); //TLS
                         
                         final String username = "sikienbmto1@gmail.com";
                         final String password = "01263500";
@@ -729,34 +731,44 @@ public class NhanVienPnl extends javax.swing.JPanel {
                                 }
                         });  
                         try {
+//                            Message message = new MimeMessage(session);
+//                            message.setFrom(new InternetAddress(username));
+//                            message.setRecipients(
+//                                Message.RecipientType.TO,
+//                                InternetAddress.parse(guiMail.txtNguoiNhan.getText())
+//                            );
+//                            message.setSubject(guiMail.txtTieuDe.getText());
+//                            message.setText(guiMail.jtaNoiDung.getText());
+//
+//                            Multipart emailContent = new MimeMultipart();
+//
+//                            MimeBodyPart texBodyPart = new MimeBodyPart();
+//                            texBodyPart.setText("cccc");
+//
+//                            MimeBodyPart pdfAtt = new MimeBodyPart();
+//                            pdfAtt.attachFile(guiMail.txtCC.getText());
+//
+//                            emailContent.addBodyPart(texBodyPart);
+//                            emailContent.addBodyPart(pdfAtt);
+//
+//                            message.setContent(emailContent);
+//
+//                            mail.queue((MimeMessage) message);
+//
+//                            guiMail.txtCC.setText("");
+//                            guiMail.txtNguoiNhan.setText("");
+//                            guiMail.txtTieuDe.setText("");
+//                            guiMail.jtaNoiDung.setText("");
                             Message message = new MimeMessage(session);
-                            message.setFrom(new InternetAddress(guiMail.txtNguoiGui.getText()));
+                            message.setFrom(new InternetAddress("EmailFromASMJava3"));
                             message.setRecipients(
                                 Message.RecipientType.TO,
                                 InternetAddress.parse(guiMail.txtNguoiNhan.getText())
                             );
                             message.setSubject(guiMail.txtTieuDe.getText());
                             message.setText(guiMail.jtaNoiDung.getText());
-
-                            Multipart emailContent = new MimeMultipart();
-
-                            MimeBodyPart texBodyPart = new MimeBodyPart();
-                            texBodyPart.setText("cccc");
-
-                            MimeBodyPart pdfAtt = new MimeBodyPart();
-                            pdfAtt.attachFile(guiMail.txtCC.getText());
-
-                            emailContent.addBodyPart(texBodyPart);
-                            emailContent.addBodyPart(pdfAtt);
-
-                            message.setContent(emailContent);
-
-                            mail.queue((MimeMessage) message);
-
-                            guiMail.txtCC.setText("");
-                            guiMail.txtNguoiNhan.setText("");
-                            guiMail.txtTieuDe.setText("");
-                            guiMail.jtaNoiDung.setText("");
+                            Transport.send(message);
+                            System.out.println("Gửi nè");
                         } catch (Exception err) {
                             JOptionPane.showMessageDialog(new Frame(), err.getMessage());
                             err.printStackTrace();
