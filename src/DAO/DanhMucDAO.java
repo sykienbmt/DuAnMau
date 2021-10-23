08eb963b3b057f7a59388b2f61b10c27a8d61498
Kien Nguyen <83683857+sykienbmt@users.noreply.github.com>
2021-10-23 14:18:18 +0700

    update GiaDichVu

--- src/DAO/DanhMucDAO.java
+++ src/DAO/DanhMucDAO.java
@@ -9,12 +9,12 @@ import model.DanhMuc;
  */
 public class DanhMucDAO extends AbsDAO<DanhMuc>{
     public List<Object[]> loadDichVu(int idDanhMuc) {
-        return getRawValues("select idDichVu,tenDichVu,tenDonVi,gia from DichVu a join DanhMuc b on a.idDanhMuc = b.idDanhMuc "
+        return getRawValues("select idDichVu,tenDichVu,tenDonVi,format(gia,'#,#') gia from DichVu a join DanhMuc b on a.idDanhMuc = b.idDanhMuc "
                             + "join donViTinh c on c.idDonVi = a.idDonVi where b.idDanhMuc ="+idDanhMuc+"");
     }
     
     public List<Object[]> loadAllDichVu() {
-        return getRawValues("select idDichVu,tenDichVu,tenDonVi,gia from DichVu a join DanhMuc b on a.idDanhMuc = b.idDanhMuc "
+        return getRawValues("select idDichVu,tenDichVu,tenDonVi,format(gia,'#,#') gia from DichVu a join DanhMuc b on a.idDanhMuc = b.idDanhMuc "
                             + "join donViTinh c on c.idDonVi = a.idDonVi ");
     }
 }
